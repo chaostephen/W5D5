e1d0004fa495652823403e65a02c0c069598527a
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-09-22 14:25:03 -0400

    finished 01

--- movie_buff/movie_buff/01_queries.rb
+++ movie_buff/movie_buff/01_queries.rb
@@ -65,7 +65,10 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
-  
+  Movie
+    .select(:id, :title)
+    .joins(:director)
+    .where("actors.name IN (?)", them)
   # Note: Directors appear in the 'actors' table.
 
 end
@@ -81,5 +84,8 @@ def movie_names_before_1940
   # improve performance for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+  Movie
+    .where('yr<1940')
+    .pluck(:title)
 
 end
